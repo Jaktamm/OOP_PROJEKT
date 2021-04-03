a94e1925d9a7cb54bd6b775503319a5a4409d96e
Jakob <jakobtamm@hotmail.com>
2021-04-03 18:39:39 +0300

    Näitan pildiga võimalust

--- src/Peaklass.java
+++ src/Peaklass.java
@@ -1,6 +1,22 @@
-public class Peaklass {
+import java.awt.*;
+import javax.swing.JFrame;
+
+public class Peaklass extends Canvas{
+
     public static void main(String[] args) {
-        int punktid = 0;
+        Peaklass m =new Peaklass();
+        JFrame f=new JFrame();
+        f.add(m);
+        f.setSize(400,400);
+        f.setVisible(true);
+    }
+
+    public void paint(Graphics g) {
+
+        Toolkit t=Toolkit.getDefaultToolkit();
+        Image i=t.getImage("pilt.jpg");
+        g.drawImage(i, 120,100,this);
+
     }
 
     public void voor(int mitmes, int punktid){
