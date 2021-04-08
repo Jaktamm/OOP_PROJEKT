9523552b65410b034ce6f4e739924dddc5f4c072
Jakob <jakobtamm@hotmail.com>
2021-04-08 14:07:23 +0300

    Viimased kommentaarid

--- src/Küsimus.java
+++ src/Küsimus.java
@@ -2,19 +2,19 @@ import java.util.List;
 
 public class Küsimus {
     private String küsimus;
-    private int vastus;
+    private int vastus;//Int-tüüpi, sest annab indeksi, mitmes vastusevariantidest õige on
     private String[] vastusevariandid;
-    private int punktid;
-    private String failinimi;
+    private int punktid;//Kui palju punkte küsimus väärt on
+    private String failinimi;//Pildiga küsimuste jaoks
 
-    public Küsimus(String küsimus, String[] vastusevariandid, int vastus, int punktid) {
+    public Küsimus(String küsimus, String[] vastusevariandid, int vastus, int punktid) {//Konstruktor
         this.küsimus = küsimus;
         this.vastus = vastus;
         this.vastusevariandid = vastusevariandid;
         this.punktid = punktid;
     }
 
-    public Küsimus(String küsimus, String[] vastusevariandid, int vastus, int punktid, String failinimi) {
+    public Küsimus(String küsimus, String[] vastusevariandid, int vastus, int punktid, String failinimi) {//Teine konstruktor piltidega küsimuste jaoks (sest sisaldab failinime)
         this.küsimus = küsimus;
         this.vastus = vastus;
         this.vastusevariandid = vastusevariandid;
@@ -22,6 +22,7 @@ public class Küsimus {
         this.failinimi = failinimi;
     }
 
+    //Kõikvõimalikud get-meetodid
     public String getKüsimus() {
         return küsimus;
     }
--- src/Peaklass.java
+++ src/Peaklass.java
@@ -3,9 +3,10 @@ import java.util.concurrent.TimeUnit;
 import javax.swing.*;
 
 public class Peaklass{
-    // exception viivituse proovimiseks
     public static void main(String[] args) throws Exception{
 
+
+        //Loome valmis kõik küsimused
         Küsimus nädalaPäev = new Küsimus("Mis päev on kaks päeva enne päeva, mis järgneb kohe päevale kolm päeva enne päeva, mis on kaks päeva pärast päeva kohe enne reedet?", new String[]{"pühapäev", "teisipäev", "neljapäev"}, 1, 10);
         Küsimus arvud = new Küsimus("Mu õde oli 10 aastat tagasi minust kaks korda vanem, nüüd on ta 26-aastane. Kui vana ma olen?",new String[]{"18-aastane", "16-aastane", "13-aastane"}, 0, 10);
         Küsimus kolmnurgad = new Küsimus("Mitu kolmnurka on pildil?", new String[]{"27","25", "17"}, 0, 10, "kolmnurgad.jpg");
@@ -19,58 +20,63 @@ public class Peaklass{
         Küsimus pealinn1 = new Küsimus("Millise riigi pealinn on Skopje?", new String[]{"Põhja-Makedoonia", "Bulgaaria", "Moldova"}, 0, 10);
         Küsimus pealinn2 = new Küsimus("Millise riigi pealinn on Bern?", new String[]{"Austria", "Šveits", "Tšehhi"}, 1, 10);
 
-
+        //Mäng toimub kahes voorus, esimeses on tekstiküsimused, teises pildiküsimused, paneme need vastavatesse massiividesse
         Küsimus[] küsimused1 = new Küsimus[]{nädalaPäev, arvud, misMaOlen, mitmesKoht, pealinn1, pealinn2};
         Küsimus[] küsimused2 = new Küsimus[]{ruudud, kolmnurgad, kuubik, blokid, lipp1, lipp2};
 
-        int punktid = 0;
+        int punktid = 0;//Algne punktiskoor
 
+        //Esimene küsimustevoor
         for (int i = 0; i < küsimused1.length; i++) {
-            voor(i+1, punktid);
-            TimeUnit.SECONDS.sleep(1);
+            voor(i+1, punktid);//Abimeetod, väljastab, mitmenda vooruga on tegemist ja kui palju mängijal punkte on
+            TimeUnit.SECONDS.sleep(1);//Väike delay sujuvama kogemuse tagamiseks
             JList list = new JList(new String[] {küsimused1[i].getVastusevariandid()[0], küsimused1[i].getVastusevariandid()[1], küsimused1[i].getVastusevariandid()[2]});
-            UIManager.put("OptionPane.minimumSize",new Dimension(800,100));
-            JOptionPane.showMessageDialog(
+            //Kasutame JOptionPane, et väljastada kõik vastusevariandid
+            UIManager.put("OptionPane.minimumSize",new Dimension(800,100));//Küsimusakna suurus
+            JOptionPane.showMessageDialog(//Kuvab küsimuse ja vastusevariandid
                     null, list, küsimused1[i].getKüsimus(), JOptionPane.PLAIN_MESSAGE);
             punktid = õigeVastus(küsimused1[i], list.getSelectedIndex(),punktid);
+            //getSelectedIndex() annab meile märku, millise vastuse mängija valis ja abimeetodi õigeVastus abiga vaatame, kas see oli õige ning liidame/lahutame vastavad punktid
         }
         System.out.println("Said esimesest plokist kokku " + punktid + " punkti!");
         System.out.println("Edasi tuleb pildiküsimuste plokk.");
 
+        //Teine (pildiküsimuste) voor
         for (int i = 0; i < küsimused2.length; i++) {
             voor(i+1+küsimused1.length, punktid);
             TimeUnit.SECONDS.sleep(2);
-            JFrame f=new JFrame();
-            f.setPreferredSize(new Dimension(800, 800));
+            JFrame f=new JFrame();//Loome JFrame, et saaks kuvada pilti
+            f.setPreferredSize(new Dimension(800, 800));//Pildiraami suurus
             f.pack();
-            String proov = küsimused2[i].getFailinimi();
+            String proov = küsimused2[i].getFailinimi();//Saab get-meetodiga failinime, milles on pilt
             f.add(new JLabel(new ImageIcon(proov)));
             f.setVisible(true);
             JList list = new JList(new String[]{küsimused2[i].getVastusevariandid()[0], küsimused2[i].getVastusevariandid()[1], küsimused2[i].getVastusevariandid()[2]});
-            if (küsimused2[i].getVastusevariandid().length == 4) {
+            //Samamoodi, nagu eelmises tsüklis, kuvame vastusevariandid
+            if (küsimused2[i].getVastusevariandid().length == 4) {//Kui vastusevariante on 4, siis muudame listi
                 list = new JList(new String[]{küsimused2[i].getVastusevariandid()[0], küsimused2[i].getVastusevariandid()[1], küsimused2[i].getVastusevariandid()[2], küsimused2[i].getVastusevariandid()[3]});
             }
-            else if (küsimused2[i].getVastusevariandid().length == 5) {
+            else if (küsimused2[i].getVastusevariandid().length == 5) {//Kui vastusevariante on 5, siis muudame listi
                 list = new JList(new String[]{küsimused2[i].getVastusevariandid()[0], küsimused2[i].getVastusevariandid()[1], küsimused2[i].getVastusevariandid()[2], küsimused2[i].getVastusevariandid()[3], küsimused2[i].getVastusevariandid()[4]});
             }
             UIManager.put("OptionPane.minimumSize",new Dimension(800,100));
-            JOptionPane.showMessageDialog(
-                    null, list, küsimused2[i].getKüsimus(), JOptionPane.PLAIN_MESSAGE);
-            f.dispose();
-            punktid = õigeVastus(küsimused2[i], list.getSelectedIndex(),punktid);
+            JOptionPane.showMessageDialog(//Kuvab küsimuse ja vastusevariandid
+                    null, list, küsimused2[i].getKüsimus(), JOptionPane.PLAIN_MESSAGE);//Küsimusakna suurus
+            f.dispose();//Sulgeb pildi
+            punktid = õigeVastus(küsimused2[i], list.getSelectedIndex(),punktid);//Kontrollib, kas vastus oli õige
         }
-        System.out.println("Said mälumängust kokku " + punktid + " punkti!");
+        System.out.println("Said mälumängust kokku " + punktid + " punkti!");//Lõppskoor
     }//main
 
 
-    public static void voor(int mitmes, int punktid){
+    public static void voor(int mitmes, int punktid){//Abimeetod
         System.out.println("Käimas on " + mitmes + ". voor!");
         System.out.println("Punktiskoor: " + punktid + " punkti");
     }
 
-    public static int õigeVastus(Küsimus küsimus, int vastus, int punktid){
-        int võimalus = 1 + (int)(Math.random()*((7-1) + 1));
-        if (küsimus.getVastus() == vastus){
+    public static int õigeVastus(Küsimus küsimus, int vastus, int punktid){//Abimeetod, tagastab uue punktide arvu vastavalt sellele, kas vastus oli õige või vale
+        int võimalus = 1 + (int)(Math.random()*((7-1) + 1));//Suvaline number 1st 7ni, et tagasiside igakord seesama ei oleks
+        if (küsimus.getVastus() == vastus){//Kui vastus on õige
             if (võimalus == 1) System.out.println("Tubli!");
             else if (võimalus == 2) System.out.println("Hea töö!");
             else if (võimalus == 3) System.out.println("LETS GO!!!");
@@ -78,9 +84,9 @@ public class Peaklass{
             else if (võimalus == 5) System.out.println("Vapustav!");
             else if (võimalus == 6) System.out.println("Õige!");
             else if (võimalus == 7) System.out.println("Õige vastus! Jätka samas vaimus!");
-            punktid += küsimus.getPunktid();
+            punktid += küsimus.getPunktid();//.. siis lisatakse küsimuse punktid
         }
-        else{
+        else{//Kui vastus on vale
             if (võimalus == 1) System.out.println("Vale!");
             else if (võimalus == 2) System.out.println("Vale vastus!");
             else if (võimalus == 3) System.out.println("Ebaõnn!");
@@ -88,9 +94,9 @@ public class Peaklass{
             else if (võimalus == 5) System.out.println("Sinu vastus on vale!");
             else if (võimalus == 6) System.out.println("Vastasid valesti!");
             else if (võimalus == 7) System.out.println("Vastus ei olnud õige!");
-            punktid -= küsimus.getPunktid();
+            punktid -= küsimus.getPunktid();//.. siis lahutatakse küsimuse punktid
         }
-        return punktid;
+        return punktid;//Tagastame uue punktiskoori
     }
 
 
